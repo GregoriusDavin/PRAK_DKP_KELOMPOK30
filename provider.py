aef9397f075ac0d95ff930c18ae62e03a090e4dc
littlemouse21 <102349241+littlemouse21@users.noreply.github.com>
2022-03-27 11:35:27 +0700

    Add files via upload

---
+++ provider.py
@@ -0,0 +1,9 @@
+def prov():
+    print("Silahkan Pilih Provider ")
+    print("1. XXI")
+    print("2. CINEPOLIS")
+    print("3. RAJAWALI")
+    print("4. Kembali <-")
+    a = int(input("Pilih : "))
+    movie(a)
+    return 0
