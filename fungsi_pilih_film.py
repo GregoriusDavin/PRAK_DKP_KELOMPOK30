c11a5b7eabbc4fbc3668b0bc3eeed7da680b3fb4
GregoriusDavin_Kelompok30 <102125620+GregoriusDavin@users.noreply.github.com>
2022-03-27 11:23:49 +0700

    pilih film

---
+++ fungsi_pilih_film.py
@@ -0,0 +1,11 @@
+def movie(a):
+    if a == 1:
+        NamaFilm()
+    elif a  == 2:
+         NamaFilm()
+    elif a == 3:
+        NamaFilm()
+    elif a == 4:
+        kota()
+    else:
+        print("TIDAK ADA DI PILIHAN MBLO")
